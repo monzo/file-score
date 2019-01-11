bfc1f7cc045ed08024c1cabed32c272436845d01
Kavi Dhokia <kavi@monzo.com>
2019-01-11 14:43:56 +1100

    Build plugin

--- file-score.sketchplugin/Contents/Sketch/symbols-count.js
+++ file-score.sketchplugin/Contents/Sketch/symbols-count.js
@@ -86,15 +86,15 @@ var exports =
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = "./src/my-command.js");
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/symbols-count.js");
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ "./src/my-command.js":
-/*!***************************!*\
-  !*** ./src/my-command.js ***!
-  \***************************/
+/***/ "./src/symbols-count.js":
+/*!******************************!*\
+  !*** ./src/symbols-count.js ***!
+  \******************************/
 /*! exports provided: default */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
@@ -148,4 +148,4 @@ module.exports = require("sketch");
 }
 that['onRun'] = __skpm_run.bind(this, 'default')
 
-//# sourceMappingURL=my-command.js.map
+//# sourceMappingURL=symbols-count.js.map
