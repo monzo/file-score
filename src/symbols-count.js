9e603a792716ab79548854ba0eec61da32e97708
Alex Pate <alexpate@monzo.com>
2019-05-15 15:48:10 +0100

    Prettier

--- src/symbols-count.js
+++ src/symbols-count.js
@@ -1,71 +1,68 @@
-import sketch from 'sketch'
+import sketch from 'sketch';
+import fs from 'fs';
 
-const doc = sketch.getSelectedDocument()
-const pages = doc.pages
+const doc = sketch.getSelectedDocument();
+const pages = doc.pages;
 
-const ignoreTypes = [
-  'Page',
-  'Artboard',
-  'Group',
-  'SymbolMaster'
-]
+const ignoreTypes = ['Page', 'Artboard', 'Group', 'SymbolMaster'];
 
 export default function() {
-  let layerCount = 0
-  let symbolsCount = 0
+  let layerCount = 0;
+  let symbolsCount = 0;
 
   pages.forEach(function countLayers(layer) {
-    const layerType = layer.type
+    const layerType = layer.type;
 
     // We want to ignore layers types defined above, slices, masks etc.
     if (shouldCountLayer(layer)) {
-      layerCount += 1
+      layerCount += 1;
     }
     // Count symbols and layers with shared styles
     if (shouldCountSymbol(layer)) {
-      symbolsCount += 1
+      symbolsCount += 1;
     }
 
     // Don't iterate through symbol masters (in case there are unlinked symbols)
-    if (layer.layers && layer.layers.length
-        && layerType != 'SymbolMaster') {
+    if (layer.layers && layer.layers.length && layerType != 'SymbolMaster') {
       // iterate through the children
-      layer.layers.forEach(countLayers)
+      layer.layers.forEach(countLayers);
     }
-  })
+  });
 
-  const symbolsPercentage = Math.round((symbolsCount / layerCount) * 100)
+  const symbolsPercentage = Math.round((symbolsCount / layerCount) * 100);
 
-  const message = (layerCount == 0) ? `Add some layers first! Get creative`
-    : `You're using ${symbolsCount} symbols (${symbolsCount}/${layerCount}) – ${symbolsPercentage}%`
-  sketch.UI.message(message)
+  const message =
+    layerCount == 0
+      ? `Add some layers first! Get creative`
+      : `You're using ${symbolsCount} symbols (${symbolsCount}/${layerCount}) – ${symbolsPercentage}%`;
+  sketch.UI.message(message);
 }
 
 function shouldCountLayer(layer) {
-  const layerType = layer.type
-  var shouldCount = true
+  const layerType = layer.type;
+  var shouldCount = true;
   // Ignored types
   if (ignoreTypes.includes(layerType)) {
-    shouldCount = false
+    shouldCount = false;
   }
   // Slices
-  if (layer.sketchObject.class() == "MSSliceLayer") {
-    shouldCount = false
+  if (layer.sketchObject.class() == 'MSSliceLayer') {
+    shouldCount = false;
   }
   // Masks (that aren't symbols)
   if (layer.sketchObject.hasClippingMask() && !layer.sharedStyleId) {
-    shouldCount = false
+    shouldCount = false;
   }
   // Shape paths that have a parent with a layer style
   if (layerType == 'ShapePath' && layer.parent.sharedStyleId) {
-    shouldCount = false
+    shouldCount = false;
   }
-  return shouldCount
+  return shouldCount;
 }
 
 function shouldCountSymbol(layer) {
   if (layer.type == 'SymbolInstance' || layer.sharedStyleId) {
-    return true
+    return true;
   }
-  return false
+  return false;
 }
